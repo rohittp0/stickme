f9b9e71628c0f066697a3bf10c421c7437346e7c
Rohit T P <tprohit9@gmail.com>
2023-05-26 23:29:58 +0530

    image captioning done

--- main.py
+++ main.py
@@ -1,18 +1,22 @@
 from typing import List, Tuple
 
-from PIL import Image
-from transformers import VisionEncoderDecoderModel, ViTImageProcessor, AutoTokenizer
+from transformers import pipeline
 
-max_length = 16
-num_beams = 4
-gen_kwargs = {"max_length": max_length, "num_beams": num_beams}
+from googleapiclient.discovery import build
+from httplib2 import Http
+from oauth2client import file, client, tools
 
-model = VisionEncoderDecoderModel.from_pretrained("nlpconnect/vit-gpt2-image-captioning")
-feature_extractor = ViTImageProcessor.from_pretrained("nlpconnect/vit-gpt2-image-captioning")
-tokenizer = AutoTokenizer.from_pretrained("nlpconnect/vit-gpt2-image-captioning")
 
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-model.to(device)
+def get_photos_client():
+    SCOPES = 'https://www.googleapis.com/auth/photoslibrary.readonly'
+
+    store = file.Storage('token-for-google.json')
+    creds = store.get()
+    if not creds or creds.invalid:
+        flow = client.flow_from_clientsecrets('client_id.json', SCOPES)
+        creds = tools.run_flow(flow, store)
+
+    return build('photoslibrary', 'v1', http=creds.authorize(Http()))
 
 
 def get_from_album(album_id: str) -> List[Tuple[str, str]]:
@@ -22,30 +26,11 @@ def get_from_album(album_id: str) -> List[Tuple[str, str]]:
     :return: A list of image urls and ids
     """
 
-    pass
-
-
-def predict_step(image_paths):
-    images = []
-    for image_path in image_paths:
-        i_image = Image.open(image_path)
-        if i_image.mode != "RGB":
-            i_image = i_image.convert(mode="RGB")
-
-        images.append(i_image)
-
-    pixel_values = feature_extractor(images=images, return_tensors="pt").pixel_values
-    pixel_values = pixel_values.to(device)
-
-    output_ids = model.generate(pixel_values, **gen_kwargs)
-
-    preds = tokenizer.batch_decode(output_ids, skip_special_tokens=True)
-    preds = [pred.strip() for pred in preds]
-    return preds
-
 
 def main():
-    text = predict_step(["https://ankur3107.github.io/assets/images/image-captioning-example.png"])
+    image_to_text = pipeline("image-to-text", model="nlpconnect/vit-gpt2-image-captioning")
+    text = image_to_text("https://ankur3107.github.io/assets/images/image-captioning-example.png")
+
     print(text)
 
 
