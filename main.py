184aa51b632258a845898aa181a4db1f1a043822
Rohit T P <tprohit9@gmail.com>
2023-05-27 18:32:47 +0530

    serve imag downloaded images

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import os
 
 import numpy as np
-from flask import Flask, request, send_from_directory
+import requests
+from flask import Flask, request, send_from_directory, send_file
 
 from caption import load_captions, get_image_by_id
 from search import load_model, get_similarity_index
@@ -26,9 +27,25 @@ def search():
     results = []
     for index in indices:
         image_id, caption = captions[index]
-        results.append({"img": get_image_by_id(image_id), "caption": caption})
+        results.append({"img": f"/image/{image_id}", "caption": caption})
 
     return results
+
+
+@app.route("/image/<image_id>")
+def image(image_id: str):
+    if os.path.exists(f"public/img/{image_id}.png"):
+        return send_file(image_id, mimetype='image/png')
+
+    image_url = get_image_by_id(image_id)
+    response = requests.get(image_url)
+    if response.status_code != 200:
+        raise Exception(f"{image_url} : Request failed with status code {response.status_code}, {response.text}")
+
+    with open(f"public/img/{image_id}.png", "wb") as f:
+        f.write(response.content)
+
+    return send_file(f"public/img/{image_id}.png", mimetype='image/png')
 
 
 @app.route('/', defaults={'path': None})
@@ -43,4 +60,4 @@ def public(path: str):
 
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=5000)
--- public/js/index.js
+++ public/js/index.js
@@ -1,9 +1,15 @@
 function getCard(img, caption)
 {
+    const p = document.createElement("p");
+    p.innerText = caption;
+
     return `<div class="card" style="width: 18rem;">
-            <img src="${img}" class="card-img-top" alt="...">
+            <img src="${img}" class="card-img-top" alt="${p.innerText}" id="${img}">
             <div class="card-body">
-                <p class="card-text">${caption}</p>
+                <p class="card-text">${p.innerText}</p>
+            </div>
+            <div class="card-footer">
+                <button class="btn btn-primary" onclick="share('${img}', \`${p.innerText}\`)">Share</button>
             </div>
         </div>`;
 }
@@ -31,5 +37,26 @@ document.getElementById("search").addEventListener("keyup", debounce((e) => {
                 });
             });
     }
-}, 300));
+}, 1000));
+
+
+const share = async (id, caption) => {
+    const img = document.getElementById(id);
+    const canvas = document.createElement('canvas');
+    canvas.width = img.naturalWidth;
+    canvas.height = img.naturalHeight;
+    canvas.getContext('2d').drawImage(img, 0, 0);
+    canvas.toBlob(function(blob) {
+        if (navigator.share) {
+            navigator.share({
+                files: [new File([blob], 'image.png', {type: blob.type})],
+                title: 'StickMe',
+                text: caption
+            })
+                .then(() => console.log('Successful share'))
+                .catch((error) => console.log('Error sharing', error));
+        }
+    });
+
+};
 
